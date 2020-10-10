e94ebe93fb859ee1f105e87933f6c46aa11ffaf1
Adrian Szymanowski <adrian.szymanowski@accenture.com>
2020-10-10 19:50:55 +0200

    Create the Feeditem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by adrian.szymanowski on 10/10/2020.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
