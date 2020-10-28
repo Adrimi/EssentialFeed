8182e6f4a5297a81a67aeb545332fb1e194783a5
Adrian Szymanowski <adrian.szymanowski@accenture.com>
2020-10-28 20:51:02 +0100

    Does not delete cache upon `LocalFeedLoader` creation

---
+++ EssentialFeedTests/Feed Cache/FeedCacheUseCaseTests.swift
@@ -0,0 +1,29 @@
+//
+//  FeedCacheUseCaseTests.swift
+//  EssentialFeedTests
+//
+//  Created by adrian.szymanowski on 28/10/2020.
+//
+
+import XCTest
+
+class LocalFeedLoader {
+    init(store: FeedStore) {
+        
+    }
+}
+
+class FeedStore {
+    var deleteCachedFeedCallCount = 0
+}
+
+class FeedCacheUseCaseTests: XCTestCase {
+    
+    func test_init_doesNotDeleteCacheUponCreation() {
+        let store = FeedStore()
+        let _ = LocalFeedLoader(store: store)
+        
+        XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
+    }
+    
+}
