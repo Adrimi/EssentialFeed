2dd6e3e24fbdb967346670cd2ffba2d71ec643aa
Adrimi <adriano100417@gmail.com>
2020-11-09 21:32:05 +0100

    added missing file

---
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -0,0 +1,62 @@
+//
+//  CodableFeedStoreTests.swift
+//  EssentialFeed
+//
+//  Created by adrian.szymanowski on 09/11/2020.
+//  
+//
+
+import XCTest
+import EssentialFeed
+
+class CodableFeedStore {
+    func retrieve(completion: @escaping FeedStore.RetrievalCompletion) {
+        completion(.empty)
+    }
+}
+
+class CodableFeedStoreTests: XCTestCase {
+
+    func test_retrieve_deliversEmptyOnEmptyCache() {
+        let sut = makeSUT()
+        let exp = expectation(description: "Wait for cache retrieval")
+        
+        sut.retrieve { result in
+            switch result {
+            case .empty:
+                break
+                
+            default:
+                XCTFail("Expected empty result, got \(result) instead")
+            }
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+    }
+    
+    func test_retrieve_deliversEmptyOnEmptyCache() {
+        let sut = makeSUT()
+        let exp = expectation(description: "Wait for cache retrieval")
+        
+        sut.retrieve { result in
+            switch result {
+            case .empty:
+                break
+                
+            default:
+                XCTFail("Expected empty result, got \(result) instead")
+            }
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+    }
+    
+    // MARK: - Helper Methods
+    private func makeSUT() -> CodableFeedStore {
+        let sut = CodableFeedStore()
+        return sut
+    }
+
+}
