746aad899dda5f0ba00534a30e03a9c2e4059c8a
Adrian Szymanowski <adriano100417@gmail.com>
2021-02-11 14:39:54 +0100

    Move `RemoteFeedImageDataLoader` to production

---
+++ EssentialFeed/Feed API/RemoteFeedImageDataLoader.swift
@@ -0,0 +1,66 @@
+//
+//  RemoteFeedImageDataLoader.swift
+//  EssentialFeed
+//
+//  Created by Adrian Szymanowski on 11/02/2021.
+//
+
+import Foundation
+
+public class RemoteFeedImageDataLoader {
+    public typealias Result = (FeedImageDataLoader.Result) -> Void
+    
+    private let client: HTTPClient
+    
+    public init(client: HTTPClient) {
+        self.client = client
+    }
+    
+    public enum Error: Swift.Error {
+        case invalidData
+    }
+    
+    private final class HTTPClientTaskWrapper: FeedImageDataLoaderTask {
+        private var completion: ((FeedImageDataLoader.Result) -> Void)?
+        var wrapped: HTTPClientTask?
+        
+        init(_ completion: @escaping (FeedImageDataLoader.Result) -> Void) {
+            self.completion = completion
+        }
+        
+        func complete(with result: FeedImageDataLoader.Result) {
+            completion?(result)
+        }
+        
+        func cancel() {
+            preventFurtherComlpetions()
+            wrapped?.cancel()
+        }
+        
+        private func preventFurtherComlpetions() {
+            completion = nil
+        }
+    }
+    
+    @discardableResult
+    public func loadImageData(from url: URL, completion: @escaping Result) -> FeedImageDataLoaderTask {
+        let task = HTTPClientTaskWrapper(completion)
+        
+        task.wrapped = client.get(from: url) { [weak self] result in
+            guard self != nil else { return }
+            
+            switch result {
+            case let .success((data, response)):
+                if response.statusCode == 200, !data.isEmpty {
+                    task.complete(with: .success(data))
+                } else {
+                    task.complete(with: .failure(Error.invalidData))
+                }
+            case let .failure(error): task.complete(with: .failure(error))
+            }
+        }
+        
+        return task
+    }
+}
+
