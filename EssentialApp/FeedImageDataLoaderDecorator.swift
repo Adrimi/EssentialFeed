c28789723ae8c249710143bf1034a5c94fc711e2
Adrian Szymanowski <adriano100417@gmail.com>
2021-02-23 11:22:45 +0100

    Move `FeedimageDataLoaderCacheDecorator` to production

---
+++ EssentialApp/FeedImageDataLoaderDecorator.swift
@@ -0,0 +1,26 @@
+//
+//  Created by Adrian Szymanowski on 23/02/2021.
+//
+
+import Foundation
+import EssentialFeed
+
+public class FeedImageDataLoaderCacheDecorator: FeedImageDataLoader {
+    private let decoratee: FeedImageDataLoader
+    private let cache: FeedImageDataCache
+    
+    public init(decoratee: FeedImageDataLoader, cache: FeedImageDataCache) {
+        self.decoratee = decoratee
+        self.cache = cache
+    }
+    
+    public func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+        decoratee.loadImageData(from: url) { [weak self] result in
+            completion(result.map { imageData in
+                self?.cache.save(imageData, for: url) { _ in }
+                return imageData
+            })
+        }
+    }
+}
+
--- EssentialAppTests/FeedImageDataLoaderCacheDecoratorTests.swift
+++ EssentialAppTests/FeedImageDataLoaderCacheDecoratorTests.swift
@@ -4,25 +4,7 @@
 
 import XCTest
 import EssentialFeed
-
-class FeedImageDataLoaderCacheDecorator: FeedImageDataLoader {
-    private let decoratee: FeedImageDataLoader
-    private let cache: FeedImageDataCache
-    
-    init(decoratee: FeedImageDataLoader, cache: FeedImageDataCache) {
-        self.decoratee = decoratee
-        self.cache = cache
-    }
-    
-    func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
-        decoratee.loadImageData(from: url) { [weak self] result in
-            completion(result.map { imageData in
-                self?.cache.save(imageData, for: url) { _ in }
-                return imageData
-            })
-        }
-    }
-}
+import EssentialApp
 
 class FeedImageDataLoaderCacheDecoratorTests: XCTestCase, FeedImageDataLoaderTestCase {
 
