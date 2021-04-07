73b2830d4fa1c06b87c3d5e7f229e41a5929af65
Adrian Szymanowski <adriano100417@gmail.com>
2021-04-07 16:14:00 +0200

    Inject calendar to make tests reliable

--- EssentialFeedTests/Helpers/SharedTestHelpers.swift
+++ EssentialFeedTests/Helpers/SharedTestHelpers.swift
@@ -24,19 +24,17 @@ extension Date {
         self + seconds
     }
     
-    func adding(days: Int) -> Date {
-        Calendar(identifier: .gregorian)
-            .date(
-                byAdding: .day,
-                value: days,
-                to: self)!
+    func adding(days: Int, calendar: Calendar = Calendar(identifier: .gregorian)) -> Date {
+        calendar.date(
+            byAdding: .day,
+            value: days,
+            to: self)!
     }
     
-    func adding(minutes: Int) -> Date {
-        Calendar(identifier: .gregorian)
-            .date(
-                byAdding: .minute,
-                value: minutes,
-                to: self)!
+    func adding(minutes: Int, calendar: Calendar = Calendar(identifier: .gregorian)) -> Date {
+        calendar.date(
+            byAdding: .minute,
+            value: minutes,
+            to: self)!
     }
 }
