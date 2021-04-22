58261f08b8d011b2a764f3d8b70c61b08275c9e8
Adrian Szymanowski <adriano100417@gmail.com>
2021-04-22 17:50:54 +0200

    Subscribe upstream store subscriptions in a background queue to avoid blocking main queue (tests still run sync in the main queue with the immediate scheduler)

--- EssentialApp/SceneDelegate.swift
+++ EssentialApp/SceneDelegate.swift
@@ -15,6 +15,12 @@ import EssentialFeediOS
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
     
+    private lazy var scheduler: AnyDispatchQueueScheduler = DispatchQueue(
+        label: "com.adrimi.infra.queue",
+        qos: .userInitiated,
+        attributes: .concurrent
+    ).eraseToAnyScheduler()
+    
     private lazy var feedURL: URL = {
         FeedEndpoint.get().url(baseURL: baseURL)
     }()
@@ -51,10 +57,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
                 selection: showComments))
     }()
     
-    convenience init(httpClient: HTTPClient, store: FeedStore & FeedImageDataStore) {
+    convenience init(httpClient: HTTPClient, store: FeedStore & FeedImageDataStore, scheduler: AnyDispatchQueueScheduler) {
         self.init()
         self.httpClient = httpClient
         self.store = store
+        self.scheduler = scheduler
     }
     
     /// Ideally you would like to call this method, but you can't. Thus all logis, that I implement are moved to `configureWindow` method
@@ -132,11 +139,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
         return localImageLoader
             .loadImageDataPublisher(from: url)
-            .fallback(to: { [httpClient] in
+            .fallback(to: { [httpClient, scheduler] in
                 httpClient
                     .getPublisher(url: url)
                     .tryMap(FeedImageDataMapper.map)
                     .caching(to: localImageLoader, using: url)
+                    .subscribe(on: scheduler)
+                    .eraseToAnyPublisher()
             })
+            .subscribe(on: scheduler)
+            .eraseToAnyPublisher()
     }
 }
