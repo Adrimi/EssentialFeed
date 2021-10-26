ccf389fff7a4937c2444b51211d71c44bdf36df7
Adrian Szymanowski <adriano100417@gmail.com>
2021-10-26 13:33:16 +0200

    Fix building on Xcode 13.0

--- EssentialAppTests/SceneDelegateTests.swift
+++ EssentialAppTests/SceneDelegateTests.swift
@@ -8,10 +8,20 @@ import EssentialFeediOS
 
 class SceneDelegateTests: XCTestCase {
     
-    func test_sceneWillConnectToSession_configuresRootViewController() {
+    func test_configureWindow_setsWindowAsKeyAndVisible() {
+        let window = UIWindowSpy()
         let sut = SceneDelegate()
-            
-        sut.window = UIWindow()
+        sut.window = window
+        
+        sut.configureWindow()
+        
+        XCTAssertEqual(window.makeKeyAndVisibleCallCount, 1, "Expected to make window key and visible")
+    }
+    
+    func test_configureWindow_configuresRootViewController() {
+        let sut = SceneDelegate()
+        sut.window = UIWindowSpy()
+        
         sut.configureWindow()
         
         let root = sut.window?.rootViewController
@@ -20,6 +30,14 @@ class SceneDelegateTests: XCTestCase {
         
         XCTAssertNotNil(rootNavigation, "Expected a navigation controller as root, got \(String(describing: root)) instead")
         XCTAssertTrue(topController is ListViewController, "Expected a feed controller as top view controller, got \(String(describing: topController)) instead")
+    }
+    
+    private class UIWindowSpy: UIWindow {
+        var makeKeyAndVisibleCallCount = 0
+        
+        override func makeKeyAndVisible() {
+            makeKeyAndVisibleCallCount = 1
+        }
     }
     
 }
